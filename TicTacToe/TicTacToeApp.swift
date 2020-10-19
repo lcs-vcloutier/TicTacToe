484d290009cbbe9197d140234c0008474206e824
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-19 19:37:53 -0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Cloutier, Vincent on 2020-10-19.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
